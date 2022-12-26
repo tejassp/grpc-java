4471195979a14aa6a120e2cf322da6dd45c3045b
tejassp <tejas.spt@gmail.com>
2022-12-26 14:54:26 +0530

    Close grpc call executor

--- benchmarks/src/main/java/io/grpc/benchmarks/qps/AsyncClient.java
+++ benchmarks/src/main/java/io/grpc/benchmarks/qps/AsyncClient.java
@@ -167,6 +167,7 @@ public class AsyncClient {
       saveHistogram(merged, config.histogramFile);
     }
     shutdown(channels);
+    grpcCallExecutor.close();
   }
 
   private SimpleRequest newRequest() {
